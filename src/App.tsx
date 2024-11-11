82c30b6294ce60cfbfc9b8614771aa0136f8a206
DootLord <shaunoman840@gmail.com>
2024-11-11 22:15:40 +0000

    Trying to figure out states and sharin them between react components.
    Changed from css to scss!

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
+import './App.scss'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const products = [
+    { title: "egg" },
+    { title: "cheese" },
+    { title: "milk" },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [food, setFood] = useState(products);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const listItems = products.map((product) => {
+        return <li key={product.title.toLocaleLowerCase()}>{product.title}</li>
+    });
+
+    const list = <div><ul>{listItems}</ul> <MyButton /></div>;
+
+    return list;
+}
+
+function MyButton() {
+    // Count the number of times the button is clicked
+    const [count, setCount] = useState(0);
+    // Store the input value
+    const [input, setInput] = useState("");
+
+
+    function inputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setInput(e.target.value);
+    }
+
+    function handleClick() {
+        setCount(count + 1);
+        setFood([...food, { title: input }]);
+    }
+
+    return (
+        <div>
+            <input type="text" onChange={inputChange}></input>
+            <button onClick={handleClick}>
+                Clicked {count} times
+            </button>
+        </div>
+
+    );
 }
 
 export default App
